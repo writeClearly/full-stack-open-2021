998ba5bca69e9afb67bc77fa9b64ac62820421d6
writeClearly <82172053+writeClearly@users.noreply.github.com>
2021-05-07 12:06:14 +0200

    Exercise 1.5 added solution

--- chapter1/courseinfo/src/App.js
+++ chapter1/courseinfo/src/App.js
@@ -7,24 +7,26 @@ Content - How to render unknown number of <p> rows without hardcoding?
 function App() {
   // Variables
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+]
 
   return (
     <div>
       <Header title={course} />
-      <Content parts = {[part1, part2, part3]} />
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+      <Content parts = {parts} />
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
     </div>
   )
 }
@@ -36,10 +38,8 @@ const Header = (props) =>{
 }
 const Content = (props) =>{
   // Retrun name of the all courses and exercises quantity
-  console.log(props.parts[0].name)
   return (
     <div>
-    {/* <p>{props.parts[0]} {props.exercises[0]} </p> */}
     <Part part = {props.parts[0].name} exercise = {props.parts[0].exercises}/>
     <Part part = {props.parts[1].name} exercise = {props.parts[1].exercises}/>
     <Part part = {props.parts[2].name} exercise = {props.parts[2].exercises}/>
@@ -47,8 +47,7 @@ const Content = (props) =>{
   )
 }
 const Part = (props) =>{
-  // Return single paragraph containg course part name and number of exercises
-  console.log(props.part)  
+  // Return single paragraph containing course part name and number of exercises
   return (
     <p>{props.part} {props.exercise}</p>
   )
