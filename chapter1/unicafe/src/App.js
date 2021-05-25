82396d88e09cd59308ececc3c5cbf3aa92cfadd3
writeClearly <82172053+writeClearly@users.noreply.github.com>
2021-05-25 18:35:12 +0200

    Exercise 1.7 added solution

--- chapter1/unicafe/src/App.js
+++ chapter1/unicafe/src/App.js
@@ -1,46 +1,69 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from 'react'
 
+//Feedback site, handles clickable rating and prints basic statistics
 const App = () => {
-  const [voteCounter, setVote] = useState(0)
-  const [goodCounter,setGood] = useState(0)
-  const [neutralCounter,setNeutral] = useState(0)
-  const [badCounter,setBad] = useState(0)
-  const handleGood = () =>{
-   setGood(goodCounter + 1)
-   setVote(voteCounter + 1)
-   console.log(goodCounter)
+  const [votesCounter, setVotes] = useState(0)
+  const [goodCounter, setGood] = useState(0)
+  const [neutralCounter, setNeutral] = useState(0)
+  const [badCounter, setBad] = useState(0)
+  const [averageScore, setAverage] = useState(0)
+  const [positiveRatio, setPositiveRatio] = useState(0) 
+  const positiveRate = "Good"
+  const neutralRate = "Neutral"
+  const negativeRate = "Bad"
+  const mainVotesCounter = "All votes"
+  const averageStat = "Average Score"
+  const percentageSign = "%"
+  const handleGood = () => {
+    setGood(goodCounter + 1)
+    setVotes(votesCounter + 1)
   }
-  const handleNeutral = () =>{
+  const handleNeutral = () => {
     setNeutral(neutralCounter + 1)
-    setVote(voteCounter + 1) 
-    console.log(neutralCounter)
-   }
-   const handleBad = () =>{
+    setVotes(votesCounter + 1)
+  }
+  const handleBad = () => {
     setBad(badCounter + 1)
-    setVote(voteCounter + 1)
-    console.log(goodCounter)
-   }
+    setVotes(votesCounter + 1)
+  }
+  useEffect(() => {
+    if ((goodCounter > 0) && (badCounter > 0)) {
+      setAverage((goodCounter - badCounter) / votesCounter)
+    }
+    if ((goodCounter > 0) && (votesCounter > 0)){
+      setPositiveRatio(goodCounter/votesCounter * 100)
+    }
+  }, [averageScore, votesCounter, goodCounter, badCounter])
+
   return (
     <div><h1>What is your feedback?</h1>
-    <p>
-      <Button text = {"Good"} handleClick = {handleGood}/>
-      <Button text = {"Neutral"} handleClick = {handleNeutral}/>
-      <Button text = {"Bad"} handleClick = {handleBad}/>
+      <p>
+        <Button text={positiveRate} handleClick={handleGood} />
+        <Button text={neutralRate} handleClick={handleNeutral} />
+        <Button text={negativeRate} handleClick={handleBad} />
       </p>
       <h2>Statistics</h2>
       <ul>
-          <p>Good:{goodCounter}</p>
-          <p>Neutral:{neutralCounter}</p>
-          <p>Bad:{badCounter}</p>
+        <StatsRow name={positiveRate} counter={goodCounter} />
+        <StatsRow name={neutralRate} counter={neutralCounter} />
+        <StatsRow name={negativeRate} counter={badCounter} />
+        <StatsRow name={mainVotesCounter} counter={votesCounter} />
+        <StatsRow name = {averageStat} counter = {averageScore}/>
+        <StatsRow name = {averageStat} counter = {positiveRatio} sign={percentageSign}/>
       </ul>
     </div>
   )
 }
- const Button =  ({text, handleClick}) =>{
-   return(
-   <button onClick = {handleClick}> {text}</button>
-   )
- }
+const Button = ({ text, handleClick }) => {
+  return (
+    <button onClick={handleClick}> {text}</button>
+  )
+}
+const StatsRow = ({ name, counter, sign }) => {
+  return (
+    <p>{name}: {counter} {sign}</p>
+  )
+}
 
 export default App;
