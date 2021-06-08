5140904096e5a2077e1e264cf2d62d76f3e3b84d
writeClearly <82172053+writeClearly@users.noreply.github.com>
2021-06-08 11:54:15 +0200

    Exercise 1.14 added solution

--- chapter1/anecdotes/src/App.js
+++ chapter1/anecdotes/src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const anecdotes = [
@@ -11,39 +11,68 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
   ]
 
-  const [counters,setCounters] = useState(Array(anecdotes.length).fill((0)))
-  // console.log(counters)
-  const [selected, setSelected] = useState(getRandomInt(anecdotes.length))
+  const [counters, setCounters] = useState(Array(anecdotes.length).fill((0))) // Create zeroed array counting votes on anecdotes
+  const [selected, setSelected] = useState(getRandomInt(anecdotes.length))    // Generate random index
   const randomLabel = "Random Quote"
   const voteLabel = "Vote"
-  // console.log("Counters: ", counters)
-  const randQuote = () =>{
+  const randQuote = () => {
     const randint = getRandomInt(anecdotes.length)
     setSelected(randint)
   }
 
-  const voteQuote = () =>{
+  const voteQuote = () => {
     //Copy counters to incremeted array, then increment vote counter
     var incremented = [...counters]
-    incremented[selected] += 1 
+    incremented[selected] += 1
     setCounters(incremented)
   }
+  const maxIndex = (arr) => {
+    // Return index of maximal element in array
+    var max = arr[0]
+    var index = 0
+    for (let i = 1; i < arr.length; i++) {
+      if (max < arr[i]) {
+        max = arr[i]
+        index = i
+      }
+    }
+    return index
+  }
+  const buttonStyle = {
+    display: "inline-block"
+  }
   return (
-    <div>
-      {anecdotes[selected]}
-      <Button text = {randomLabel} onClick = {randQuote}/>
-      <Button text = {voteLabel} onClick = {voteQuote}/>
-      <p>This quote has: {counters[selected]} votes</p>
-    </div>
+    // To small app to worth it refactor into separate components
     
+    <div> {/*Main div*/}
+      <h2>Anecdote of the day</h2>
+      <div>{anecdotes[selected]}</div>  {/*Current quote*/}
+      <div style={buttonStyle}>         {/*Get random quote*/}
+        <Button text={randomLabel} onClick={randQuote} />
+      </div>
+      <div style={buttonStyle}>         {/*Vote on current quote*/}
+        <Button text={voteLabel} onClick={voteQuote} />
+      </div>
+
+      <div>{/*Most voted anecdote*/}
+      <p>This quote has: {counters[selected]} votes</p>
+      <div>
+        <h2>Anecdote with most votes</h2>
+        <div>{anecdotes[maxIndex(counters)]}</div>      {/*Print anecodete with most votes*/}
+        </div>
+      <p>Max votes: {counters[maxIndex(counters)]}</p>  {/*Print anecdote max score*/}
+      </div>
+    </div>
+
   )
 }
-const Button  = ({text, onClick}) =>{
+const Button = ({ text, onClick }) => {
   return (
-    <div><button onClick = {onClick}>{text}</button></div>
+    <div><button onClick={onClick}>{text}</button></div>
   )
 }
-function getRandomInt(max){
+function getRandomInt(max) {
+  // Generate random integer from range 1..n-1
   return Math.floor(Math.random() * max)
 }
 export default App
