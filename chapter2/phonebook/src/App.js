39269c25794c4fcad6229e2e6e51b922c2348920
writeClearly <82172053+writeClearly@users.noreply.github.com>
2021-06-25 09:42:48 +0200

    Exercise 2.8 and 2.9 added solution

--- chapter2/phonebook/src/App.js
+++ chapter2/phonebook/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Jim" }, { name: "Jane" }])
+  const [persons, setPersons] = useState([{ name: "Ada", phone:'124',  searched:true }, 
+                                          { name: "Jane",phone:'1312', searched:true },
+                                          { name: "Bill",phone:'1312', searched:true }])
   const [newPerson, setNewPerson] = useState([])
-
+  const [newPhone, setNewPhone] = useState([])
   const handleAddNote = (event) => {
     // handles adding note after clicking on submit add
     event.preventDefault()                                //Disable reload onclick
@@ -13,19 +15,38 @@ const App = () => {
       window.alert(`${newPerson} is already in phonebook`)
         return
       }
-    setPersons(persons.concat({ name: newPerson }))
+    if (newPhone.length === 0)
+      return
+    setPersons(persons.concat({ name: newPerson, phone: newPhone, searched: true }))
     setNewPerson([])
+    setNewPhone([])
   }
-  const handleInput = (event) => {
+  const handleSearchName = (event) =>{
+    // dynamically print only searched person
+
+    let searchedString = event.target.value.toLowerCase()
+    let searchedList = persons.map(p =>{
+      if(p.name.toLowerCase().indexOf(searchedString)!==-1){ // -1 === string not present in any substring
+        return {...p, searched:true}
+      }
+      else
+      return {...p, searched:false}
+    })
+    setPersons(searchedList)
+  }
+  const handleNameInput = (event) => {
     setNewPerson(event.target.value)
+  }
+  const handlePhoneInput = (event)=>{
+    setNewPhone(event.target.value)
   }
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>search for: <input onChange = {handleSearchName}/></div>
       <form>
-        <div>
-          name: <input onChange={handleInput} />
-        </div>
+        <div>name: <input onChange={handleNameInput} /></div>
+        <div>number: <input onChange={handlePhoneInput}/></div>
         <div>
           <button type="submit" onClick={handleAddNote}>add</button>
         </div>
@@ -37,9 +58,11 @@ const App = () => {
     </div>)
 }
 const Phonebook = ({persons}) =>{
+  // Print PersonName and Phone if currently searched
   return(
     <div>
-      {persons.map((person) => <li>{person.name}</li>)}
+      {persons.map((person) => 
+                person.searched ===true ? <li>{person.name} {person.phone}</li>:"")}
     </div>
   )
 }
--- chapter2/phonebook/src/index.js
+++ chapter2/phonebook/src/index.js
@@ -5,19 +5,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
-
-
-
-
-
-
-
-
-
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// ReactDOM.render(
-//     <App />,
-//   document.getElementById('root')
-// );
