33fa7164755924a2b924ebe5f66fd86b417edba5
writeClearly <82172053+writeClearly@users.noreply.github.com>
2021-08-29 12:50:14 +0200

    Exercise 4.5 added solution [Test favouriteBlog]

--- chapter4/bloglist/utils/list_helper.js
+++ chapter4/bloglist/utils/list_helper.js
@@ -5,7 +5,16 @@ const totalLikes = (blogs) => {
   const reducer = (accumulator, currentLikes) => accumulator + currentLikes.likes
   return blogs.reduce(reducer, 0)
 }
+const favouriteBlog = (blogs) => {
+  if(blogs.length > 0){
+    const mostLiked = blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+    return mostLiked
+  }
+  else
+    return []
+}
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
