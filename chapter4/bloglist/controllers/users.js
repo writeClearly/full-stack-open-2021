d2ebd814cb54f108dc31e2b39133ce91897712e8
writeClearly <82172053+writeClearly@users.noreply.github.com>
2021-09-13 09:29:51 +0200

    Exercise 4.18 added solution [Token authentication]

--- chapter4/bloglist/controllers/users.js
+++ chapter4/bloglist/controllers/users.js
@@ -2,26 +2,43 @@ const bcrypt = require("bcrypt") // for storing passowrds on disk
 const usersRouter = require("express").Router()
 const logger = require("../utils/logger")
 const User = require("../models/user")
-usersRouter.post("/", (request, response) => {
-  const saltRounds = 13
-  bcrypt.hash(request.body.password, saltRounds, (error, hashedPassword) => {
-    logger.info(hashedPassword)
-    // bcrypt.compare("password1234", hashedPassword).then((result)=>console.log(result))
-    const user = {
-      "username" : request.body.username,
-      "name" : request.body.name,
-      "password" : request.body.password
-    }
-    const newUser = User(user)
-    logger.info(newUser)
-    newUser.save()  
+const jwt = require("jsonwebtoken")
+const SALT_ROUNDS = 13
+
+usersRouter.post("/", async (request, response) => {
+  logger.info(request.body.password)
+  const hashedPassword = await bcrypt.hash(request.body.password, SALT_ROUNDS)
+  logger.info(hashedPassword)
+  const newUser = new User({
+    username : request.body.username,
+    name : request.body.name,
+    passwordHash : hashedPassword
   })
-  logger.info(request.body)
-  response.json(request.body)
+  await newUser.save()  
+  logger.info(newUser)
+  response.json(newUser)
 })
 
 usersRouter.get("/", async (request, response) => {
   const users = await User.find({}).populate("blogpost", {title:1, url:1, author:1}) // {title:1} means include property in response
   response.json(users)
+})
+
+usersRouter.post("/login", async (request, response) => {
+  const body = request.body
+  console.log(body.password)
+  const userForLogin = await User.findOne({username:body.username})
+  console.log(userForLogin)
+  const isPasswordCorrect = await bcrypt.compare(body.password, userForLogin.passwordHash)
+  if(!isPasswordCorrect){
+    response.sendStatus(401)
+    return
+  }
+  const userForToken = {
+    username : userForLogin.username,
+    id : userForLogin._id
+  }
+  const token = jwt.sign(userForToken, process.env.SECRET)
+  response.status(200).send({token, username : userForLogin.username, name : userForLogin.name})
 })
 module.exports = usersRouter
